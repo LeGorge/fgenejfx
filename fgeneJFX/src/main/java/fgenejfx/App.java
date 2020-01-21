7d5a3d4019da056d04d3c1c5bd4dd8a24b20d7e1
LeGorge <andersongmoura@gmail.com>
2020-01-21 00:10:46 -0300

    setting up javaFX

---
+++ fgeneJFX/src/main/java/fgenejfx/App.java
@@ -0,0 +1,14 @@
+package fgenejfx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    // TODO Auto-generated method stub
+
+  }
+
+}
